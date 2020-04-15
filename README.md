2947dec3fb150bc6d3204075431e1a96ddde5b28
vasilki <lukanikhin@mail.ru>
2020-04-15 20:43:51 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# stm_ssd1306
+Connecting OLED display ssd1306 to the Nucleo F401RE
+
+Pinout:
+OLED:STM32
+GND:GND
+VCC:5V
+D0:SCK/D13(CN5)
+D1:MOSI/D11 (CN5)
+RES connected to VCC
+DC:D7(CN9)
+CS:CS/D10(CN5)
+
+Environment:
+STM32 NUCLEO-F401RE
+OLED display ssd1306
+Linux Mint 18.2 Sonya
+STM32Cube_1.0 version 4.24.0.Repository is STM32Cube_FW_F4_V1.19.0.
+System Workbench for STM32 (Version: 1.13.2.201703061529) based on Eclipse IDE version Mars.2.Release 4.5.2
---
+++ Src/main_usercode.c
@@ -0,0 +1,192 @@
+#include "stm32f4xx_hal.h"
+#include <string.h>
+#include <stdio.h>
+//#include "adc.h"
+#include "button_handle.h"
+#include "timers.h"
+#include "uart.h"
+#include "dwt_stm32_delay.h"
+#include "PF_Config.h"
+#include "LIB_Config.h"
+
+extern UART_HandleTypeDef huart1; /*declared in main.c*/
+//extern ADC_HandleTypeDef hadc1; /*declared in main.c*/
+extern TIM_HandleTypeDef htim9;
+extern SPI_HandleTypeDef hspi1;
+
+
+static void main_Init(void);
+static void main_heartbeat(void);
+static void main_draw(void);
+static void main_buttons(void);
+
+
+void main_usercode(void)
+{
+
+  uint8_t loc_buff[20];
+  unsigned int loc_time;
+  unsigned int loc_time_ms = 0;
+  unsigned int loc_time_sec = 0;
+  static unsigned int loc_prev_time_ms=0;
+  static unsigned int loc_prev_time_sec=0;
+  uint8_t loc_srbyte = 1;
+
+  main_Init();
+
+  tim_UpdatePeriod();
+  loc_time = tim_GetPeriod();
+  loc_time_sec = tim_GetTimeFromStartSEC();
+  
+  
+  /*HeartBeat*/
+  main_heartbeat();
+  main_draw();
+  /*Running LED*/
+  if(loc_time_sec != loc_prev_time_sec)
+  {
+
+  }
+  else
+  {
+    /*nothing to do*/
+  }
+
+  //loc_adc_val = adc_GetValue(&hadc1);
+
+
+  loc_prev_time_sec = loc_time_sec;
+  loc_prev_time_ms = loc_time_ms;
+  
+  return;
+}
+
+
+
+void main_Init(void)
+{
+  static uint8_t loc_B_IsFirstTime = 0;
+
+  if(loc_B_IsFirstTime == 0)
+  {
+    /*BUTTON init*/
+    button_SetActiveButtons('C',13);
+    button_SetActiveButtons('B',6);
+
+    /*TIM init*/
+    tim_InitTimer(&htim9);
+
+    /*DWT init*/
+    DWT_Delay_Init();
+    
+    /*UART init*/
+    uart_Init(&huart1);
+    uart_PrintfBuildVersion(&huart1);
+    
+    /*TFT2.8 init*/
+    system_init(&hspi1);
+
+
+    loc_B_IsFirstTime = 1;
+  }
+  else
+  {
+    /*nothing to do*/
+  }
+
+
+  return;
+}
+
+
+void main_heartbeat(void)
+{
+  uint32_t loc_time_sec;
+  static uint32_t loc_prev_time_sec = 0;
+
+  loc_time_sec = tim_GetTimeFromStartSEC();
+  
+  if(loc_prev_time_sec != loc_time_sec)
+  {
+    UART_PRINTFINTEGER(loc_time_sec,"DEC")
+
+
+      
+    /*
+     * LED2 is occupied by SCI
+    if((loc_time_sec % 2) == 0)
+    {
+      HAL_GPIO_WritePin(GPIOA,GPIO_PIN_5,GPIO_PIN_SET);
+    }
+    else
+    {
+      HAL_GPIO_WritePin(GPIOA,GPIO_PIN_5,GPIO_PIN_RESET);
+    }
+    */
+    loc_prev_time_sec = loc_time_sec;
+  }
+  else
+  {
+    /*nothing to do*/
+  }
+  
+  
+  return;
+}
+
+
+void main_draw(void)
+{
+
+  lcd_draw_rect(1, 1, 50, 50, RED);
+
+  lcd_draw_rect(30, 40, 150, 100, RED);
+  lcd_draw_circle(120, 160, 50, BLUE);
+  lcd_draw_line(30, 40, 180, 140, RED);
+
+  lcd_draw_line(30, 220, 210, 240, RED);
+  lcd_draw_line(30, 220, 120, 280, RED);
+  lcd_draw_line(120, 280, 210, 240, RED);
+
+
+  return;
+}
+
+void main_buttons()
+{
+  unsigned char loc_B_button_state = 0;
+  
+  button_Processing();
+  loc_B_button_state = button_GetButtonState('B',6);
+  if(loc_B_button_state != 0)
+  {
+    HAL_GPIO_WritePin(GPIOB,GPIO_PIN_5,GPIO_PIN_SET);
+  }
+  else
+  {
+    HAL_GPIO_WritePin(GPIOB,GPIO_PIN_5,GPIO_PIN_RESET);
+  }
+  
+  return;
+}
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
